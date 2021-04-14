d792341e7b2f50a3a688363d54cce3d090909964
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-04-14 21:17:39 +0000

    Fixed env, signal, betty, and more comments.

--- countword.c
+++ countword.c
@@ -1,7 +1,7 @@
 #include "header.h"
 /**
  * countword - counts the number of words in a string, per delimiter
- * @str: string to cound words in
+ * @str: string to count words
  * @delim: what to delimit words by, usually a space
  * Return: returns total number of words
  */
@@ -10,7 +10,7 @@ size_t countword(char *str, char delim)
 	size_t i = 0, count = 0;
 
 	while (str[i])
-	{
+	{/*if no nullbyte, then won't count last word*/
 		if (str[i] == delim || str[i + 1] == '\0')
 		{
 			count++;
--- shell_main.c
+++ shell_main.c
@@ -1,9 +1,10 @@
 #include "header.h"
 /**
- * main - main function, takes input, find command and executes it
- * @ac: unused in main, count of arguments made
- * @av: unused in main, arguments made
+ * main - main function, takes input, find command and calls execve()
  * Return: returns 0, or exits via input
+ * 
+ * Description: EVN and EXIT implementation. Pass data through tokenizer
+ * and runs function to execute commands
  */
 int main(void)
 {
@@ -11,41 +12,48 @@ int main(void)
 	size_t bufsize = 0;
 	int i_mode = 1;
 
-
+	signal(SIGINT, handler);
 	while (i_mode)
 	{
-		if(isatty(0))
+		if (isatty(0))
 			write(STDOUT_FILENO, "shellie$ ", 9);
 		if (getline(&input, &bufsize, stdin) == EOF)
+		{
+			write(STDOUT_FILENO, "\n", 1);
 			break;
+		}
 		if (input == NULL)
 			exit(0);
 		if (_strcmp(input, "env\n\0") == 0)
 		{
-			free(input);
-			printenv();
+			free(input), input = NULL, printenv();
 			continue;
 		}
 		if (_strcmp(input, "exit\n\0") == 0)
-		{
 			break;
-		}
-		/* this is where we call our tokenizer function */
-		cmnds = tokenizer(input, " ", 0);
-/* cmnds will be used to search for the shell command to execute in execve */
-		path = pathhandle(cmnds);
-		if (cmnds == NULL)
+		cmnds = tokenizer(input, " ", 0); /*cmnds via tokenizer()*/
+		path = pathhandle(cmnds); /*get path via tokenized cmnds*/
+		if (cmnds == NULL) /*if NULL isn't found do all the free*/
 		{
-			free(input);
-			free(cmnds);
-			cmnds = NULL;
+			free(input), free(cmnds);
+			cmnds = NULL, input = NULL;
 			continue;
 		}
-		executecmd(cmnds, path, input);
+		executecmd(cmnds, path, input); /*exe cmnds found*/
 		freetokens(cmnds);
 		free(input);
 		input = NULL;
 	}
 	free(input);
 	return (0);
+}
+/*
+ *
+ *
+ **/
+void handler(int num)
+{
+	(void)num;
+	write(STDOUT_FILENO, "\n", 1);
+	write(STDOUT_FILENO, "shellie$ ", 9);	
 }
